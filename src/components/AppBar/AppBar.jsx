6be44887a8a83bdb2a746554807fa4ef4ac69a49
Anton <antonklim6@gmail.com>
2024-05-14 19:32:26 +0200

    made rep

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+
+
+export default function AppBar () {
+  return (
+    <>
+      {/* <div>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/">Tasks</NavLink>
+      </div>
+      <div>
+        <p>
+          Welcome, username<NavLink to="/">Logout</NavLink>
+        </p>
+      </div>
+      <div>
+        <NavLink to="/">Register</NavLink>
+        <NavLink to="/">Log In</NavLink>
+      </div> */}
+    </>
+  )
+}
